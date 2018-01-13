a2ca2b0e637e1616686aac2eb44f89b04b724e62
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-13 18:25:29 +0200

    redone flex box design

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,7 +11,7 @@ export class MenuComponent implements OnInit {
 
   menu: Array<{ id: number, icon: string, name: string }> = Array(
     { 'id': 0, 'icon': 'home', 'name': 'Home' },
-    { 'id': 1, 'icon': 'view_stream', 'name': 'WorkFlow' },
+    { 'id': 1, 'icon': 'view_stream', 'name': 'Workflow' },
     { 'id': 2, 'icon': 'show_chart', 'name': 'Statistic' }
     { 'id': 3, 'icon': 'perm_contact_calendar', 'name': 'Calendar' }
     { 'id': 4, 'icon': 'perm_identity', 'name': 'Users' },
