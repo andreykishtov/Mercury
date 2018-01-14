65ec2adb81a81056ca0da5d9c6c1ccb43b626060
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-14 09:47:02 +0200

    redone sizing

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -7,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  messages: Array<{ id: number, description: string, days: string, fromWho: string }> = Array(
-    { 'id': 0, 'description': 'Hey You! It’s me again :-) I attached new (...)', days: '5 minutes ago', fromWho: 'Nina Jones' },
-    { 'id': 1, 'description': 'Hey! I attached some new PSD files for (...)', days: 'About 20 Hours ago', fromWho: 'Nina Jones' },
-    { 'id': 2, 'description': 'Good morning, you are fired!', days: '2 days ago', fromWho: 'James Smith' },
-    { 'id': 3, 'description': 'Hello! Could You bring me coffee please?', days: 'about 2 weeks ago', fromWho: 'James Smith' }
+  messages: Array<{ id: number, description: string, days: string, fromWho: string, unread: string }> = Array(
+    { 'id': 0,
+     'description': 'Hey You! It’s me again :-) I attached new (...)', days: '5 minutes ago', fromWho: 'Nina Jones', unread: 'unread' },
+    { 'id': 1,
+     'description': 'Hey! I attached some new PSD files for (...)', days: 'About 20 Hours ago', fromWho: 'Nina Jones', unread: 'unread' },
+    { 'id': 2,
+     'description': 'Good morning, you are fired!', days: '2 days ago', fromWho: 'James Smith', unread: '' },
+    { 'id': 3,
+     'description': 'Hello! Could You bring me coffee please?', days: 'about 2 weeks ago', fromWho: 'James Smith', unread: '' }
 );
 
   constructor() { }
