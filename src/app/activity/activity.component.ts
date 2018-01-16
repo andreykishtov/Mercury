e399b9e70f3ace27b37a2451da44f71741ea3048
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-16 18:34:34 +0200

    created images and create count number changed color number image to flex

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -7,15 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
 
-  activitys: Array<{ id: number, name: string, action: string, days: string, fromWho: string }> = Array(
+  activitys: Array<{ id: number,
+  name: string, action: string, days: string, fromWho: string, imageUrl: string }> = Array(
     { 'id': 0,
-     name: 'Free UI Kit', 'action': 'added a new project', days: '5 days delay', fromWho: 'Nina Jones' },
+      name: 'Free UI Kit',
+      'action': 'added a new project',
+      days: '5 days delay',
+      fromWho: 'Nina Jones',
+      imageUrl: 'https://robohash.org/a' },
     { 'id': 1,
-     name: 'Free PSD Template', 'action': 'commented project', days: '2 days delay', fromWho: 'James Smith' },
+      name: 'Free PSD Template',
+      'action': 'commented project',
+      days: '2 days delay',
+      fromWho: 'James Smith',
+      imageUrl: 'https://robohash.org/b' },
     { 'id': 2,
-     name: 'Symu Website', 'action': 'completed task', days: '5 days left', fromWho: 'Alex Clooney' },
+      name: 'Symu Website',
+      'action': 'completed task',
+      days: '5 days left',
+      fromWho: 'Alex Clooney',
+      imageUrl: 'https://robohash.org/c' },
     { 'id': 3,
-     name: 'Free PSD (...)', 'action': 'added a new project', days: '2 days left', fromWho: 'Alexandra Spears' }
+      name: 'Free PSD (...)',
+      'action': 'added a new project',
+      days: '2 days left',
+      fromWho: 'Alexandra Spears',
+      imageUrl: 'https://robohash.org/a' }
 );
 
 
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -7,15 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  messages: Array<{ id: number, description: string, days: string, fromWho: string, unread: string }> = Array(
+  messages: Array<{ id: number,
+                    description: string,
+                    days: string,
+                    fromWho: string,
+                    unread: string,
+                    imageUrl: string }> = Array(
     { 'id': 0,
-     'description': 'Hey You! It’s me again :-) I attached new (...)', days: '5 minutes ago', fromWho: 'Nina Jones', unread: 'unread' },
+     'description': 'Hey You! It’s me again :-) I attached new (...)',
+      days: '5 minutes ago',
+      fromWho: 'Nina Jones',
+      unread: 'unread',
+      imageUrl: 'https://robohash.org/asdasdw2' },
     { 'id': 1,
-     'description': 'Hey! I attached some new PSD files for (...)', days: 'About 20 Hours ago', fromWho: 'Nina Jones', unread: 'unread' },
+     'description': 'Hey! I attached some new PSD files for (...)',
+      days: 'About 20 Hours ago',
+      fromWho: 'Nina Jones',
+      unread: 'unread',
+      imageUrl: 'https://robohash.org/asda2132s2' },
     { 'id': 2,
-     'description': 'Good morning, you are fired!', days: '2 days ago', fromWho: 'James Smith', unread: '' },
+     'description': 'Good morning, you are fired!',
+      days: '2 days ago',
+      fromWho: 'James Smith',
+      unread: '',
+      imageUrl: 'https://robohash.org/asda23232311sdw2' },
     { 'id': 3,
-     'description': 'Hello! Could You bring me coffee please?', days: 'about 2 weeks ago', fromWho: 'James Smith', unread: '' }
+      'description': 'Hello! Could You bring me coffee please?',
+      days: 'about 2 weeks ago',
+      fromWho: 'James Smith',
+      unread: '',
+      imageUrl: 'https://robohash.org/asdaasd222dw2' }
 );
 
   constructor() { }
