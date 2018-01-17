e6dcc92520f85916e893378ba64fed3409724343
Andrey Kishtov <andreykishtov@gmail.com>
2018-01-17 09:40:56 +0200

    trying diffrent approach

--- src/app/activity/activity.component.ts
+++ src/app/activity/activity.component.ts
@@ -14,25 +14,25 @@ export class ActivityComponent implements OnInit {
       'action': 'added a new project',
       days: '5 days delay',
       fromWho: 'Nina Jones',
-      imageUrl: 'https://robohash.org/a' },
+      imageUrl: `https://i.pinimg.com/736x/2b/ab/d6/2babd6371bb1ce1126da699da2285d19--long-brunette-hairstyles-medium-length-hairstyles.jpg` },
     { 'id': 1,
       name: 'Free PSD Template',
       'action': 'commented project',
       days: '2 days delay',
       fromWho: 'James Smith',
-      imageUrl: 'https://robohash.org/b' },
+      imageUrl: 'https://pbs.twimg.com/profile_images/606861746800721921/QuNuX5m7.jpg' },
     { 'id': 2,
       name: 'Symu Website',
       'action': 'completed task',
       days: '5 days left',
       fromWho: 'Alex Clooney',
-      imageUrl: 'https://robohash.org/c' },
+      imageUrl: 'https://www.aboutface.ca/wp-content/uploads/avatars/19/867672cb9a85519c5e5d8999d23cf254-bpfull.jpg' },
     { 'id': 3,
       name: 'Free PSD (...)',
       'action': 'added a new project',
       days: '2 days left',
       fromWho: 'Alexandra Spears',
-      imageUrl: 'https://robohash.org/a' }
+      imageUrl: 'https://www.aboutface.ca/wp-content/uploads/avatars/19/867672cb9a85519c5e5d8999d23cf254-bpfull.jpg' }
 );
 
 
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -18,25 +18,25 @@ export class MessagesComponent implements OnInit {
       days: '5 minutes ago',
       fromWho: 'Nina Jones',
       unread: 'unread',
-      imageUrl: 'https://robohash.org/asdasdw2' },
+      imageUrl: 'https://i.pinimg.com/736x/2b/ab/d6/2babd6371bb1ce1126da699da2285d19--long-brunette-hairstyles-medium-length-hairstyles.jpg' },
     { 'id': 1,
      'description': 'Hey! I attached some new PSD files for (...)',
       days: 'About 20 Hours ago',
       fromWho: 'Nina Jones',
       unread: 'unread',
-      imageUrl: 'https://robohash.org/asda2132s2' },
+      imageUrl: 'https://i.pinimg.com/736x/2b/ab/d6/2babd6371bb1ce1126da699da2285d19--long-brunette-hairstyles-medium-length-hairstyles.jpg' },
     { 'id': 2,
      'description': 'Good morning, you are fired!',
       days: '2 days ago',
       fromWho: 'James Smith',
       unread: '',
-      imageUrl: 'https://robohash.org/asda23232311sdw2' },
+      imageUrl: 'https://pbs.twimg.com/profile_images/606861746800721921/QuNuX5m7.jpg' },
     { 'id': 3,
       'description': 'Hello! Could You bring me coffee please?',
       days: 'about 2 weeks ago',
       fromWho: 'James Smith',
       unread: '',
-      imageUrl: 'https://robohash.org/asdaasd222dw2' }
+      imageUrl: 'https://pbs.twimg.com/profile_images/606861746800721921/QuNuX5m7.jpg' }
 );
 
   constructor() { }
