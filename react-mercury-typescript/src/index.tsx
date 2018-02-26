a26bd44ec0b437e90cf6d34228f911c64cd3fdc5
Andrey Kishtov <andreykishtov@gmail.com>
2018-02-26 15:27:16 +0200

    finished store

--- react-mercury-typescript/src/index.tsx
+++ react-mercury-typescript/src/index.tsx
@@ -1,11 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Hello from './components/Hello';
+import Hello from './containers/Hello';
+import { createStore } from 'redux';
+import { StoreState } from './types/index';
+import { enthusiasm } from './reducers/index';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<Hello name="TypeScript" enthusiasmLevel={10} />, document.getElementById(
-  'root'
-) as HTMLElement);
+const store = createStore<StoreState>(enthusiasm, {
+  enthusiasmLevel: 1,
+  languageName: 'TypeScript'
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
+);
 
 registerServiceWorker();
