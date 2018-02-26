e65bf9a14000830ccd7bdec40763599170cbea16
Andrey Kishtov <andreykishtov@gmail.com>
2018-02-26 13:30:44 +0200

    container

--- react-mercury-typescript/src/components/Hello.tsx
+++ react-mercury-typescript/src/components/Hello.tsx
@@ -4,9 +4,11 @@ import './Hello.css';
 export interface Props {
   name: string;
   enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
@@ -14,10 +16,13 @@ function Hello({ name, enthusiasmLevel = 1 }: Props) {
   return (
     <div className="hello">
       <div className="greeting">Hello {name + getExclamationMarks(enthusiasmLevel)}</div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
+      </div>
     </div>
   );
 }
-
 export default Hello;
 
 // helpers
@@ -25,21 +30,3 @@ export default Hello;
 function getExclamationMarks(numChars: number) {
   return Array(numChars + 1).join('!');
 }
-
-// class Hello extends React.Component<Props, object> {
-//   render() {
-//     const { name, enthusiasmLevel = 1 } = this.props;
-
-//     if (enthusiasmLevel <= 0) {
-//       throw new Error('You could be a little more enthusiastic. :D');
-//     }
-
-//     return (
-//       <div className="hello">
-//         <div className="greeting">
-//           Hello {name + getExclamationMarks(enthusiasmLevel)}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
